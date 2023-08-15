d86a9ae0a26190319df6e5848cf6deb2b16abc82
halibobo1205 <halibobo1205@gmail.com>
2023-08-15 19:11:52 +0800

    for config

--- framework/src/main/java/org/tron/core/config/args/DynamicArgs.java
+++ framework/src/main/java/org/tron/core/config/args/DynamicArgs.java
@@ -7,13 +7,13 @@ import java.io.File;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.List;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.tron.common.es.ExecutorServiceManager;
 import org.tron.common.parameter.CommonParameter;
 import org.tron.core.Constant;
 import org.tron.core.config.Configuration;
@@ -27,11 +27,13 @@ public class DynamicArgs {
 
   private long lastModified = 0;
 
-  private ScheduledExecutorService reloadExecutor = Executors.newSingleThreadScheduledExecutor();
+  private ScheduledExecutorService reloadExecutor;
+  private final String esName = "dynamic-reload";
 
   @PostConstruct
   public void init() {
     if (parameter.isDynamicConfigEnable()) {
+      reloadExecutor = ExecutorServiceManager.newSingleThreadScheduledExecutor(esName);
       logger.info("Start the dynamic loading configuration service");
       long checkInterval = parameter.getDynamicConfigCheckInterval();
       File config = getConfigFile();
@@ -111,7 +113,6 @@ public class DynamicArgs {
 
   @PreDestroy
   public void close() {
-    logger.info("Closing the dynamic loading configuration service");
-    reloadExecutor.shutdown();
+    ExecutorServiceManager.shutdownAndAwaitTermination(reloadExecutor, esName);
   }
 }
