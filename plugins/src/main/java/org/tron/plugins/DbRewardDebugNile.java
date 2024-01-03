bc509f2bab6acea5733a3e3a5c0084000a603bf5
halibobo1205 <halibobo1205@gmail.com>
2024-01-03 14:44:04 +0800

    add reward-debug cal by cycle

--- plugins/src/main/java/org/tron/plugins/DbRewardDebugNile.java
+++ plugins/src/main/java/org/tron/plugins/DbRewardDebugNile.java
@@ -1,6 +1,7 @@
 package org.tron.plugins;
 
 
+import java.math.BigInteger;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 import java.util.stream.LongStream;
@@ -30,7 +31,12 @@ public class DbRewardDebugNile implements Callable<Integer> {
   @CommandLine.Option(names = {"-h", "--help"}, help = true, description = "display a help message")
   private boolean help;
 
+  @CommandLine.Option(names = {"--cycle"})
+  private int cycle = -1;
+
   private DBInterface delegationStore;
+
+  private static final BigInteger DECIMAL_OF_VI_REWARD = BigInteger.valueOf(10).pow(18);
 
 
   @Override
@@ -55,15 +61,75 @@ public class DbRewardDebugNile implements Callable<Integer> {
   }
 
   private void accumulateWitnessReward(byte[] witness) {
-    LongStream.range(208102, 219324).forEach(c -> getWitnessVi(c, witness));
+    if (cycle > 0) {
+      accumulateWitnessVi(cycle, witness);
+    } else {
+      LongStream.range(208102, 219324).forEach(c -> getWitnessVi(c, witness));
+    }
+
   }
 
-  private void getWitnessVi(long cycle, byte[] address) {
+
+  public void accumulateWitnessVi(long cycle, byte[] address) {
+    BigInteger preVi = getWitnessVi(cycle - 1, address);
+    long voteCount = getWitnessVote(cycle, address);
+    logger.info("cycle:{} ,voteCount:{}", cycle, voteCount);
+    long reward = getReward(cycle, address);
+    logger.info("cycle:{} ,reward:{}", cycle, reward);
+    if (reward == 0 || voteCount == 0) { // Just forward pre vi
+      if (!BigInteger.ZERO.equals(preVi)) { // Zero vi will not be record
+        setWitnessVi(cycle, address, preVi);
+      }
+    } else { // Accumulate delta vi
+      BigInteger deltaVi = BigInteger.valueOf(reward)
+          .multiply(DECIMAL_OF_VI_REWARD)
+          .divide(BigInteger.valueOf(voteCount));
+      setWitnessVi(cycle, address, preVi.add(deltaVi));
+    }
+  }
+
+  private void setWitnessVi(long cycle, byte[] address, BigInteger preVi) {
+    logger.info("cycle:{} ,vi:{}", cycle, ByteArray.toHexString(preVi.toByteArray()));
+  }
+
+  private BigInteger getWitnessVi(long cycle, byte[] address) {
     byte[] v = delegationStore.get(buildViKey(cycle, address));
     logger.info("cycle:{} ,vi:{}", cycle, ByteArray.toHexString(v));
+    if (v == null) {
+      return BigInteger.ZERO;
+    } else {
+      return new BigInteger(v);
+    }
   }
 
   private byte[] buildViKey(long cycle, byte[] address) {
     return (cycle + "-" + Hex.toHexString(address) + "-vi").getBytes();
+  }
+
+  private long getReward(long cycle, byte[] address) {
+    byte[] reward = delegationStore.get(buildRewardKey(cycle, address));
+    if (reward == null) {
+      return 0L;
+    } else {
+      return ByteArray.toLong(reward);
+    }
+  }
+
+  private long getWitnessVote(long cycle, byte[] address) {
+    byte[] vote = delegationStore.get(buildVoteKey(cycle, address));
+    if (vote == null) {
+      return 0;
+    } else {
+      return ByteArray.toLong(vote);
+    }
+  }
+
+
+  private byte[] buildVoteKey(long cycle, byte[] address) {
+    return (cycle + "-" + Hex.toHexString(address) + "-vote").getBytes();
+  }
+
+  private byte[] buildRewardKey(long cycle, byte[] address) {
+    return (cycle + "-" + Hex.toHexString(address) + "-reward").getBytes();
   }
 }
