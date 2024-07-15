653a259de2e8cb3b14409507bf032c47ae1e8a39
halibobo1205 <halibobo1205@gmail.com>
2024-07-15 12:00:59 +0800

    feat(math): add log for different ret

--- chainbase/src/main/java/org/tron/common/math/Maths.java
+++ chainbase/src/main/java/org/tron/common/math/Maths.java
@@ -3,6 +3,7 @@ package org.tron.common.math;
 import com.google.common.primitives.Bytes;
 import java.nio.ByteBuffer;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.tron.common.context.GlobalContext;
@@ -10,6 +11,7 @@ import org.tron.core.store.MathStore;
 import org.tron.core.store.StrictMathStore;
 
 @Component
+@Slf4j(topic = "math")
 public class Maths {
 
   private static Optional<MathStore> mathStore = Optional.empty();
@@ -41,10 +43,21 @@ public class Maths {
       byte[] key = Bytes.concat(longToBytes(h), new byte[]{Op.POW.code},
           doubleToBytes(a), doubleToBytes(b));
       double strictResult = StrictMath.pow(a, b);
+      if (Double.compare(result, strictResult) != 0) {
+        logger.info("{}/t{}/t/{}/{}/{}/{}", h, Op.POW.code, doubleToHex(a), doubleToHex(b),
+            doubleToHex(result), doubleToHex(strictResult));
+      }
       mathStore.ifPresent(s -> s.put(key, doubleToBytes(result)));
       strictMathStore.ifPresent(s -> s.put(key, doubleToBytes(strictResult)));
     });
     return result;
+  }
+
+  private static String doubleToHex(double input) {
+    // Convert the starting value to the equivalent value in a long
+    long doubleAsLong = Double.doubleToRawLongBits(input);
+    // and then convert the long to a hex string
+    return Long.toHexString(doubleAsLong);
   }
 
   private static byte[] doubleToBytes(double value) {
