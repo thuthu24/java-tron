29b64998e67c9674f5fb8cdae2f3666ef33d4a89
halibobo1205 <halibobo1205@gmail.com>
2024-07-23 11:20:31 +0800

    feat(toolkit): add db query

--- plugins/src/main/java/org/tron/plugins/Db.java
+++ plugins/src/main/java/org/tron/plugins/Db.java
@@ -14,6 +14,7 @@ import picocli.CommandLine;
         DbCopy.class,
         DbRoot.class,
         DbCheckpointCompare.class,
+        DbQuery.class,
     },
     commandListHeading = "%nCommands:%n%nThe most commonly used db commands are:%n"
 )
