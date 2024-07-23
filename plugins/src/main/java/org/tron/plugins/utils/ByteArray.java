8dc93b69790275a4942a63bf68d41a67a46e5a5b
halibobo1205 <halibobo1205@gmail.com>
2024-07-23 15:07:03 +0800

    feat(tools): fix cannot find type for Any url when using JSONFormat.printer()

--- plugins/src/main/java/org/tron/plugins/utils/ByteArray.java
+++ plugins/src/main/java/org/tron/plugins/utils/ByteArray.java
@@ -4,18 +4,118 @@ import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.MessageOrBuilder;
+import com.google.protobuf.TypeRegistry;
 import com.google.protobuf.util.JsonFormat;
 import java.math.BigInteger;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.bouncycastle.util.encoders.Hex;
 import org.tron.protos.Protocol;
+import org.tron.protos.contract.AccountContract;
+import org.tron.protos.contract.AssetIssueContractOuterClass;
+import org.tron.protos.contract.BalanceContract;
+import org.tron.protos.contract.ExchangeContract;
+import org.tron.protos.contract.MarketContract;
+import org.tron.protos.contract.ProposalContract;
+import org.tron.protos.contract.ShieldContract;
+import org.tron.protos.contract.SmartContractOuterClass;
+import org.tron.protos.contract.StorageContract;
+import org.tron.protos.contract.VoteAssetContractOuterClass;
+import org.tron.protos.contract.WitnessContract;
+
 
 public class ByteArray {
 
   public static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
   public static final byte[] ZERO_BYTE_ARRAY = new byte[]{0};
   public static final int WORD_SIZE = 32;
+
+  public static JsonFormat.Printer PRINTER;
+
+  static {
+    TypeRegistry.Builder registry = TypeRegistry.newBuilder();
+    // see ContractType
+    registry.add(AccountContract.AccountCreateContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.TransferContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(AssetIssueContractOuterClass.TransferAssetContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(VoteAssetContractOuterClass.VoteAssetContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(WitnessContract.VoteWitnessContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(WitnessContract.WitnessCreateContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(AssetIssueContractOuterClass.AssetIssueContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(WitnessContract.WitnessUpdateContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(AssetIssueContractOuterClass.ParticipateAssetIssueContract
+        .getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(AccountContract.AccountUpdateContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.FreezeBalanceContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.UnfreezeBalanceContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.WithdrawBalanceContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(AssetIssueContractOuterClass.UnfreezeAssetContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(AssetIssueContractOuterClass.UpdateAssetContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(ProposalContract.ProposalCreateContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(ProposalContract.ProposalApproveContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(ProposalContract.ProposalDeleteContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(AccountContract.SetAccountIdContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(SmartContractOuterClass.CreateSmartContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(SmartContractOuterClass.TriggerSmartContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(SmartContractOuterClass.UpdateSettingContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(ExchangeContract.ExchangeCreateContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(ExchangeContract.ExchangeInjectContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(ExchangeContract.ExchangeWithdrawContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(ExchangeContract.ExchangeTransactionContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(SmartContractOuterClass.UpdateEnergyLimitContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(AccountContract.AccountPermissionUpdateContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(SmartContractOuterClass.ClearABIContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(StorageContract.UpdateBrokerageContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(ShieldContract.ShieldedTransferContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(MarketContract.MarketSellAssetContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(MarketContract.MarketCancelOrderContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.FreezeBalanceV2Contract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.UnfreezeBalanceV2Contract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.WithdrawExpireUnfreezeContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.DelegateResourceContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.UnDelegateResourceContract.getDefaultInstance()
+        .getDescriptorForType());
+    registry.add(BalanceContract.CancelAllUnfreezeV2Contract.getDefaultInstance()
+        .getDescriptorForType());
+    PRINTER = JsonFormat.printer().usingTypeRegistry(registry.build()).sortingMapKeys();
+  }
 
   /**
    * get bytes data from string data.
@@ -129,7 +229,7 @@ public class ByteArray {
 
   public static String toJson(MessageOrBuilder messageOrBuilder) {
     try {
-      return JsonFormat.printer().sortingMapKeys().print(messageOrBuilder);
+      return PRINTER.print(messageOrBuilder);
     } catch (InvalidProtocolBufferException e) {
       throw new RuntimeException(e);
     }
