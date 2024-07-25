23e45265c198d5ca3308409ff86ea1e35414c392
halibobo1205 <halibobo1205@gmail.com>
2024-07-25 14:18:27 +0800

    feat(state-root): add log for write state root

--- chainbase/src/main/java/org/tron/core/store/CheckPointV2Store.java
+++ chainbase/src/main/java/org/tron/core/store/CheckPointV2Store.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.tron.common.storage.WriteOptionsWrapper;
-import org.tron.common.utils.ByteArray;
 import org.tron.common.utils.Pair;
 import org.tron.common.utils.Sha256Hash;
 import org.tron.core.db.TronDatabase;
@@ -90,8 +89,7 @@ public class CheckPointV2Store extends TronDatabase<byte[]> {
   public void updateByBatch(Map<byte[], byte[]> rows, WriteOptionsWrapper writeOptions) {
     Pair<Optional<Long>, Sha256Hash> ret = RootHashService.getRootHash(rows);
     super.updateByBatch(rows, writeOptions);
-    ret.getKey().ifPresent(height -> stateRootStore.put(ByteArray.fromLong(height),
-        ret.getValue().getBytes()));
+    ret.getKey().ifPresent(height -> stateRootStore.put(height, ret.getValue()));
   }
 
 }
--- chainbase/src/main/java/org/tron/core/store/CheckTmpStore.java
+++ chainbase/src/main/java/org/tron/core/store/CheckTmpStore.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.tron.common.storage.WriteOptionsWrapper;
-import org.tron.common.utils.ByteArray;
 import org.tron.common.utils.Pair;
 import org.tron.common.utils.Sha256Hash;
 import org.tron.core.db.TronDatabase;
@@ -61,7 +60,6 @@ public class CheckTmpStore extends TronDatabase<byte[]> {
   public void updateByBatch(Map<byte[], byte[]> rows, WriteOptionsWrapper writeOptions) {
     Pair<Optional<Long>, Sha256Hash> ret = RootHashService.getRootHash(rows);
     super.updateByBatch(rows, writeOptions);
-    ret.getKey().ifPresent(height -> stateRootStore.put(ByteArray.fromLong(height),
-        ret.getValue().getBytes()));
+    ret.getKey().ifPresent(height -> stateRootStore.put(height, ret.getValue()));
   }
 }
